2975b43b2db581d1d3eb419e09a3fdd14a307322
Gab Estremos <gab.estremos@whitecloak.com>
2019-10-17 08:49:10 +0800

    joy's clean archi

--- Movies/controller/movieController.js
+++ Movies/controller/movieController.js
@@ -2,44 +2,21 @@ const express = require('express');
 
 const router = express.Router();
 
+const checkJwt = require('../middleware/jwt');
+
 const movieService = require('../services/movieService');
 
-const getAllMovies = router.get('/home', movieService.getAllMovies);
-const getMovie = router.get('/movie/:id', movieService.getMovie);
-const getMoviesbyGenre = router.get(
-  '/movie/genres/:genre',
-  movieService.getMoviesbyGenre
-);
-const getCountriesofMovie = router.get(
-  '/movie/:id/countries',
-  movieService.getCountriesofMovie
-);
-const getWritersofMovie = router.get(
-  '/movie/:id/writers',
-  movieService.getWritersofMovie
-);
-const getMoviesbyWriter = router.get(
-  '/writers',
-  movieService.getMoviesbyWriter
-);
-const getSearchedMovies = router.get('/search', movieService.getSearchedMovies);
-const deleteMovie = router.get('/delete/:id', movieService.deleteMovie);
-const getUpdateMovie = router.get('/update/:id', movieService.getUpdateMovie);
-const putUpdateMovie = router.put('/update/:id', movieService.putUpdateMovie);
-const redirectHome = router.get('/', movieService.redirectHome);
-const invalidToken = router.get(movieService.invalidToken);
+router.get('/', movieService.redirectHome);
+router.get(movieService.invalidToken);
+router.get('/home', movieService.getAllMovies);
+router.get('/movie/:id', movieService.getMovie);
+router.get('/movie/genres/:genre', movieService.getMoviesbyGenre);
+router.get('/movie/:id/countries', movieService.getCountriesofMovie);
+router.get('/movie/:id/writers', movieService.getWritersofMovie);
+router.get('/writers', movieService.getMoviesbyWriter);
+router.get('/search', movieService.getSearchedMovies);
+router.get('/delete/:id', checkJwt, movieService.deleteMovie);
+router.get('/update/:id', checkJwt, movieService.getUpdateMovie);
+router.put('/update/:id', checkJwt, movieService.putUpdateMovie);
 
-module.exports = {
-  getAllMovies,
-  getMoviesbyGenre,
-  getMovie,
-  getCountriesofMovie,
-  getWritersofMovie,
-  getMoviesbyWriter,
-  getSearchedMovies,
-  deleteMovie,
-  getUpdateMovie,
-  putUpdateMovie,
-  redirectHome,
-  invalidToken
-};
+module.exports = router;
--- Movies/routes/movies.js
+++ Movies/routes/movies.js
@@ -6,23 +6,9 @@ const movieRouter = express.Router();
 // eslint-disable-next-line node/no-extraneous-require
 const bodyParser = require('body-parser');
 
-const checkJwt = require('../middleware/jwt');
-
 const movieController = require('../controller/movieController');
 
 movieRouter.use(bodyParser.json());
-
-movieRouter.use(movieController.invalidToken);
-movieRouter.use(movieController.redirectHome);
-movieRouter.use(movieController.getAllMovies);
-movieRouter.use(movieController.getMoviesbyGenre);
-movieRouter.use(movieController.getMovie);
-movieRouter.use(movieController.getCountriesofMovie);
-movieRouter.use(movieController.getWritersofMovie);
-movieRouter.use(movieController.getMoviesbyWriter);
-movieRouter.use(movieController.getSearchedMovies);
-movieRouter.use(checkJwt, movieController.deleteMovie);
-movieRouter.use(checkJwt, movieController.getUpdateMovie);
-movieRouter.use(checkJwt, movieController.putUpdateMovie);
+movieRouter.use(movieController);
 
 module.exports = movieRouter;
